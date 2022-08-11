f722aebde1cd0bceaa5193dabd5ffa808de109ed
Chuan <chuancc@google.com>
2022-08-11 17:23:28 +0800

    added k3s on gce

---
+++ hack/k3s-quickstart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 0. Create SA with proper roles
+
+# 1. Create instance template for server & agent
+
+# 2. Create MIG for server & agent group
+
+# 3. Install K3s server 
+
+# 4. Install k3s agent & register to server
