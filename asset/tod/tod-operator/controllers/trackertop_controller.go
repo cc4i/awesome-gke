72610df67ff82fd1a003caecf814ed415f3060c9
Chuan <chuancc@google.com>
2022-09-23 21:43:46 +0800

    added serviceaccout with role

--- asset/tod/tod-operator/controllers/trackertop_controller.go
+++ asset/tod/tod-operator/controllers/trackertop_controller.go
@@ -23,6 +23,7 @@ import (
 
 	appv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -68,9 +69,70 @@ func (r *TrackerTopReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	}
 
 	// Reconcile Tracker' Deployment & Service
+	tsa := buildServiceAccount(tto.Spec.Where)
+	foundServiceAccount := &corev1.ServiceAccount{}
+	err := r.Get(ctx, types.NamespacedName{Name: tsa.Name, Namespace: tsa.Namespace}, foundServiceAccount)
+	if err != nil && errors.IsNotFound(err) {
+		//SA
+		l.Info("Creating ServiceAccount for Tracker", "serviceaccount", tsa.Name)
+		if err := ctrl.SetControllerReference(&tto, tsa, r.Scheme); err != nil {
+			l.Error(err, "Unable to Set OwnerReferences to Tracker's ServiceAccount", "tsa", tsa)
+			return ctrl.Result{}, err
+		}
+		if err = r.Create(ctx, tsa); err != nil {
+			l.Error(err, "Unable to create ServiceAccount for Tracker", "tsa", tsa)
+			return ctrl.Result{}, err
+		}
+		//Role
+		tr := buildRole(tsa)
+		l.Info("Creating Role for ServiceAccount", "role", tr.Name)
+		if err := ctrl.SetControllerReference(&tto, tr, r.Scheme); err != nil {
+			l.Error(err, "Unable to Set OwnerReferences to Role", "tr", tr)
+			return ctrl.Result{}, err
+		}
+		if err = r.Create(ctx, tr); err != nil {
+			l.Error(err, "Unable to create Role", "tr", tr)
+			return ctrl.Result{}, err
+		}
+		//RoleBinding
+		trb := buildRoleBinding(tr, tsa)
+		l.Info("Creating RoleBinding for ServiceAccount", "rolebinding", trb.Name)
+		if err := ctrl.SetControllerReference(&tto, trb, r.Scheme); err != nil {
+			l.Error(err, "Unable to Set OwnerReferences to RoleBinding", "trb", trb)
+			return ctrl.Result{}, err
+		}
+		if err = r.Create(ctx, trb); err != nil {
+			l.Error(err, "Unable to create RoleBinding", "tr", tr)
+			return ctrl.Result{}, err
+		}
+		//ClusterRole
+		tcr := buildClusterRole(tsa)
+		l.Info("Creating ClusterRole for ServiceAccount", "clusterrole", tcr.Name)
+		if err := ctrl.SetControllerReference(&tto, tcr, r.Scheme); err != nil {
+			l.Error(err, "Unable to Set OwnerReferences to ClusterRole", "tcr", tcr)
+			return ctrl.Result{}, err
+		}
+		if err = r.Create(ctx, tcr); err != nil {
+			l.Error(err, "Unable to create ClusterRole", "tcr", tcr)
+			return ctrl.Result{}, err
+		}
+		//ClusterRoleBinding
+		tcrb := buildClusterRoleBinding(tcr, tsa)
+		l.Info("Creating ClusterRoleBinding for ServiceAccount", "clusterrolebinding", tcrb.Name)
+		if err := ctrl.SetControllerReference(&tto, tcrb, r.Scheme); err != nil {
+			l.Error(err, "Unable to Set OwnerReferences to ClusterRoleBinding", "tcrb", tcrb)
+			return ctrl.Result{}, err
+		}
+		if err = r.Create(ctx, tcrb); err != nil {
+			l.Error(err, "Unable to create RoleBinding", "tr", tr)
+			return ctrl.Result{}, err
+		}
+
+	}
+
 	for _, tracker := range tto.Spec.Trackers {
 
-		td := buildTrackerDeploy(tracker, tto.Spec.Redis, tto.Spec.Where)
+		td := buildTrackerDeploy(tracker, tsa, tto.Spec.Redis, tto.Spec.Where)
 		foundDeployment := &appv1.Deployment{}
 		err := r.Get(ctx, types.NamespacedName{Name: td.Name, Namespace: td.Namespace}, foundDeployment)
 		if err != nil && errors.IsNotFound(err) {
@@ -175,7 +237,8 @@ func (r *TrackerTopReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	return ctrl.Result{}, nil
 }
 
-func buildTrackerDeploy(tracker trackerv1.Tracker, redis trackerv1.ThirdParty, ns string) *appv1.Deployment {
+func buildTrackerDeploy(tracker trackerv1.Tracker, tsa *corev1.ServiceAccount, redis trackerv1.ThirdParty, ns string) *appv1.Deployment {
+	terminate := int64(60)
 	td := appv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      tracker.Name,
@@ -197,6 +260,8 @@ func buildTrackerDeploy(tracker trackerv1.Tracker, redis trackerv1.ThirdParty, n
 					},
 				},
 				Spec: corev1.PodSpec{
+					TerminationGracePeriodSeconds: &terminate,
+					ServiceAccountName:            tsa.GetName(),
 					Containers: []corev1.Container{
 						{
 							Name:  "tracker",
@@ -395,6 +460,105 @@ func buildRedisService(redis trackerv1.ThirdParty, ns string) *corev1.Service {
 		},
 	}
 	return &rs
+}
+
+func buildServiceAccount(ns string) *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "tracker-sa",
+			Namespace: ns,
+		},
+	}
+}
+func buildRole(tsa *corev1.ServiceAccount) *rbacv1.Role {
+	return &rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tsa.GetName() + "-role",
+			Namespace: tsa.GetNamespace(),
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{""},
+				Resources: []string{"nodes", "services", "pods", "endpoints"},
+				Verbs:     []string{"get", "list", "watch"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{"configmaps"},
+				Verbs:     []string{"get"},
+			},
+			{
+				APIGroups: []string{"extensions"},
+				Resources: []string{"deployments"},
+				Verbs:     []string{"get", "list", "watch"},
+			},
+		},
+	}
+}
+func buildRoleBinding(tr *rbacv1.Role, tsa *corev1.ServiceAccount) *rbacv1.RoleBinding {
+	return &rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tr.Name + "-rolebinding",
+			Namespace: tr.Namespace,
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     tr.Name,
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind: "ServiceAccount",
+				Name: tsa.Name,
+			},
+		},
+	}
+}
+
+func buildClusterRole(tsa *corev1.ServiceAccount) *rbacv1.ClusterRole {
+	return &rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tsa.GetName() + "-clusterrole",
+			Namespace: tsa.GetNamespace(),
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{""},
+				Resources: []string{"nodes", "services", "pods", "endpoints"},
+				Verbs:     []string{"get", "list", "watch"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{"configmaps"},
+				Verbs:     []string{"get"},
+			},
+			{
+				APIGroups: []string{"extensions"},
+				Resources: []string{"deployments"},
+				Verbs:     []string{"get", "list", "watch"},
+			},
+		},
+	}
+}
+func buildClusterRoleBinding(tcr *rbacv1.ClusterRole, tsa *corev1.ServiceAccount) *rbacv1.ClusterRoleBinding {
+	return &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tcr.Name + "-clusterrolebinding",
+			Namespace: tcr.Namespace,
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     tcr.Name,
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      tsa.Name,
+				Namespace: tsa.Namespace,
+			},
+		},
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
