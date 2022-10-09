4abfa76c8c271f09ca8ba6aaf608513ddc0114a7
Chuan <chuancc@google.com>
2022-10-09 11:17:32 +0800

    added session recording

--- asset/tod/tracker/tcp/echo.go
+++ asset/tod/tracker/tcp/echo.go
@@ -1,6 +1,7 @@
 package tcp
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -15,12 +16,23 @@ type echoServer struct {
 	eng       gnet.Engine
 	addr      string
 	multicore bool
+	sessions  map[string]string
 }
 
 func (es *echoServer) OnBoot(eng gnet.Engine) gnet.Action {
 	es.eng = eng
-	log.Info().Msgf("echo server with multi-core=%t is listening on %s\n", es.multicore, es.addr)
+	log.Info().Msgf("TCP Server with multi-core=%t is listening on %s\n", es.multicore, es.addr)
 	return gnet.None
+}
+
+func (es *echoServer) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
+	remote := c.RemoteAddr()
+	log.Info().Msgf("Connection from %s", remote.String())
+	if es.sessions == nil {
+		es.sessions = make(map[string]string)
+	}
+	es.sessions[remote.String()] = remote.Network()
+	return []byte{}, gnet.None
 }
 
 func (es *echoServer) OnTraffic(c gnet.Conn) gnet.Action {
@@ -29,6 +41,20 @@ func (es *echoServer) OnTraffic(c gnet.Conn) gnet.Action {
 	if string(buf) == "bye\r\n" {
 		log.Info().Msg("Received break signal and disconnected with client.")
 		c.Close()
+	}
+	switch string(buf) {
+	case "bye":
+	case "bye\r\n":
+		log.Info().Msg("Received break signal and disconnected with client.")
+		c.Close()
+		return gnet.None
+	case "list":
+	case "list\r\n":
+		log.Info().Msg("All clients:")
+		buf, _ := json.Marshal(es.sessions)
+		c.Write(buf)
+		return gnet.None
+
 	}
 	// built response message
 	node := os.Getenv("POD_NODE_NAME")
